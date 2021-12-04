daaae6816e8c87925c96399cade35e47ad247660
Aliocha AMERGE <aliocha@pop-os.localdomain>
2021-12-04 20:31:12 +0100

    empty project, should be well configured

---
+++ backend/src/main/java/fr/su/bankapp/Application.java
@@ -0,0 +1,18 @@
+package fr.su.bankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    /**
+     * Application's main method
+     *
+     * @param args the console arguments
+     */
+    public static void main(String[] args) {
+        // Run the app!
+        SpringApplication.run(Application.class, args);
+    }
+}
