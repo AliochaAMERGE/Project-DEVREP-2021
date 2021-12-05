10d80eaff5f16344bdc595d805b556b9515eb239
Aliocha AMERGE <aliocha@pop-os.localdomain>
2021-12-05 15:33:57 +0100

    seems to be broken

--- frontend/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
-
-createApp(App).use(router).use(store).mount('#app');
