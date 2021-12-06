3b3b0f6a81412954793bfb9f00094dd82910aa5c
Aliocha AMERGE <aliocha.amerge@hotmail.com>
2021-12-06 15:16:57 +0100

    reboot again...

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,11 @@
 <template>
-  <div id="app">
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
   </div>
+  <router-view/>
 </template>
+
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
