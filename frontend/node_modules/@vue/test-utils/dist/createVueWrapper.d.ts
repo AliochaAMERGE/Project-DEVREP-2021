a5d4a87966cb4fd800a9944ae9f72e2e777f43f4
Aliocha AMERGE <aliocha.amerge@hotmail.com>
2021-12-08 23:51:48 +0100

    time has come

--- frontend/node_modules/@vue/test-utils/dist/createVueWrapper.d.ts
+++
@@ -1,3 +0,0 @@
-import { ComponentPublicInstance, App } from 'vue';
-import { VueWrapper } from './vueWrapper';
-export declare function createWrapper<T extends ComponentPublicInstance>(app: App | null, vm: ComponentPublicInstance, setProps?: (props: Record<string, unknown>) => void): VueWrapper<T>;
